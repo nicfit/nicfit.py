d881285cbada88011678ef90ccaa5a0f42a5d72a
Travis Shirk <travis@pobox.com>
2016-12-18 17:37:19 -0700

    messin with stuff

--- cookiecutter/{{cookiecutter.project_name}}/tests/test_{{cookiecutter.py_module}}.py
+++ cookiecutter/{{cookiecutter.project_name}}/tests/test_{{cookiecutter.py_module}}.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-test_{{ cookiecutter.project_slug }}
+test_{{ cookiecutter.py_module }}
 ----------------------------------
 
-Tests for `{{ cookiecutter.project_slug }}` module.
+Tests for `{{ cookiecutter.py_module }}` module.
 """
 
 {% if cookiecutter.use_pytest == "yes" -%}
@@ -13,24 +13,24 @@ import pytest
 import unittest
 from nose.tools import *
 {% endif %}
-import {{ cookiecutter.project_slug }}
+import {{ cookiecutter.py_module }}
 {% if cookiecutter.use_pytest != "yes" -%}
 
 
-class Test{{ cookiecutter.project_slug|capitalize }}(unittest.TestCase):
+class Test{{ cookiecutter.py_module|capitalize }}(unittest.TestCase):
 
     def setUp(self):
         pass
 
     def test_metadata(self):
-        assert({{ cookiecutter.project_slug }}.__name__)
-        assert({{ cookiecutter.project_slug }}.__author__)
-        assert({{ cookiecutter.project_slug }}.__author_email__)
-        assert({{ cookiecutter.project_slug }}.__version__)
-        assert({{ cookiecutter.project_slug }}.__version_info__)
-        assert({{ cookiecutter.project_slug }}.__release__)
-        assert({{ cookiecutter.project_slug }}.__license__)
-        assert({{ cookiecutter.project_slug }}.__version_txt__)
+        assert({{ cookiecutter.py_module }}.__name__)
+        assert({{ cookiecutter.py_module }}.__author__)
+        assert({{ cookiecutter.py_module }}.__author_email__)
+        assert({{ cookiecutter.py_module }}.__version__)
+        assert({{ cookiecutter.py_module }}.__version_info__)
+        assert({{ cookiecutter.py_module }}.__release__)
+        assert({{ cookiecutter.py_module }}.__license__)
+        assert({{ cookiecutter.py_module }}.__version_txt__)
 
     def tearDown(self):
         pass
