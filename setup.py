31d73a962a2f49bdb6e17c492f6017f3e33d5acd
Travis Shirk <travis@pobox.com>
2016-12-27 23:29:03 -0700

    Advert 3.4 support

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers = [
     'Natural Language :: English',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.4',
 ]
 
 
