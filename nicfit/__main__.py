83744ae455d97e47b1b26ce10ba6a32697c0c54d
Travis Shirk <travis@pobox.com>
2016-12-27 22:30:27 -0700

    Clean up

--- nicfit/__main__.py
+++ nicfit/__main__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from nicfit import Application
-from . import __version__
+from . import version
 
 
 def main(args):
     print("\m/")
 
 
-app = Application(main, version=__version__)
+app = Application(main, version=version)
 if __name__ == "__main__":
     app.run()
--- tests/test_nicfit.py
+++ tests/test_nicfit.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import pytest
 import nicfit
 """
 test_nicfit
