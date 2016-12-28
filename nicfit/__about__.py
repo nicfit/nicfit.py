9031f5342cc945b58c5f784347adc82947146dcd
Travis Shirk <travis@pobox.com>
2016-12-28 00:10:48 -0700

    Update

--- nicfit/__about__.py
+++ nicfit/__about__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import namedtuple
 
-__version__ = '0.4.0-beta'
+__version__ = '0.4.0'
 __release_name__ = ""
 __release__ = __version__.split('-')[1] if '-' in __version__ else "final"
 
@@ -10,14 +10,14 @@ __project_slug__ = 'nicfit.py'
 __author__ = 'Travis Shirk'
 __author_email__ = 'travis@pobox.com'
 __url__ = 'https://github.com/nicfit/nicfit.py'
-__description__ = 'Common Python utils (App, logging, config, etc.)'
+__description__ = 'Common Python utils (App, logging, config, etc.) [Python3.4]'
 
 __version_info__ = \
     namedtuple("Version", "major, minor, maint, release")(
         *(tuple((int(v) for v in __version__.split('-')[0].split('.'))) +
           tuple((__release__,))))
 
-__years__ = "<YEAR>"
+__years__ = "2016-2017"
 __license__ = 'MIT'
 
 __version_txt__ = """
