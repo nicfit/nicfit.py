8bdb739a5b70702ebf43025ff35ced8d071ec9d2
Travis Shirk <travis@pobox.com>
2017-02-04 16:52:33 -0700

    new: dev: unshallowed_repo fixture (for Travis-CI shallow clones)

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pytest
+from pathlib import Path
+
+
+@pytest.fixture
+def unshallowed_repo():
+    src = "./.git/shallow_file"
+    bak = src + ".BACKUP"
+
+    shallow_file = Path(src)
+    if shallow_file.exists():
+        shallow_file.rename(bak)
+        yield shallow_file
+        shallow_file = Path(bak).rename(src)
+    else:
+        yield None
