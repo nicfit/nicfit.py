adef1e19c0181954c71551179326e250d85ba06b
Travis Shirk <travis@pobox.com>
2018-05-14 22:30:29 -0600

    Release updates

--- nicfit/__about__.py
+++ nicfit/__about__.py
@@ -14,7 +14,7 @@ def __parse_version(v):                                       # pragma: nocover
     return ver, rel, ver_info
 
 
-__version__ = "0.8"
+__version__ = "0.8.1"
 __release_name__ = ""
 __years__ = "2016-2018"
 
