5197d0d4350400a2090050543ec5276bb07aa5c5
Travis Shirk <travis@pobox.com>
2019-07-30 19:15:46 -0600

    Parcyling

--- nicfit/__about__.py
+++ nicfit/__about__.py
@@ -1,37 +1,18 @@
-from collections import namedtuple
+import dataclasses
 
+project_name = "nicfit.py"
+version      = "0.8.4"
+release_name = ""
+author       = "Travis Shirk"
+author_email = "travis@pobox.com"
+years        = "2016-2019"
 
-def __parse_version(v):                                       # pragma: nocover
-    ver, rel = v, "final"
-    for c in ("a", "b", "c"):
-        parsed = v.split(c)
-        if len(parsed) == 2:
-            ver, rel = (parsed[0], c + parsed[1])
+@dataclasses.dataclass
+class Version:
+    major: int
+    minor: int
+    maint: int
+    release: str
+    release_name: str
 
-    v = tuple((int(v) for v in ver.split(".")))
-    ver_info = namedtuple("Version", "major, minor, maint, release")(
-        *(v + (tuple((0,)) * (3 - len(v))) + tuple((rel,))))
-    return ver, rel, ver_info
-
-
-__version__ = "0.8.4"
-__release_name__ = ""
-__years__ = "2016-2019"
-
-_, __release__, __version_info__ = __parse_version(__version__)
-__project_name__ = "nicfit.py"
-__project_slug__ = "nicfit.py"
-__pypi_name__ = "nicfit.py"
-__author__ = "Travis Shirk"
-__author_email__ = "travis@pobox.com"
-__url__ = "https://github.com/nicfit/nicfit.py"
-__description__ = "Common Python utils (App, logging, config, etc.)"
-__long_description__ = ""
-__license__ = "MIT"
-__github_url__ = "https://github.com/nicfit/nicfit.py",
-__version_txt__ = """
-%(__name__)s %(__version__)s (C) Copyright %(__years__)s %(__author__)s
-This program comes with ABSOLUTELY NO WARRANTY! See LICENSE for details.
-Run with --help/-h for usage information or read the docs at
-%(__url__)s
-""" % (locals())
+version_info = Version(0, 8, 4, "final", "")
--- setup.py
+++ setup.py
@@ -1,98 +1,20 @@
 #!/usr/bin/env python
-import io
-import os
-import re
-import sys
-import warnings
-from pathlib import Path
+from parcyl import Setup, find_package_files
 
-import parcyl
-
-classifiers = [
-    "Intended Audience :: Developers",
-    "Operating System :: POSIX",
-    "Natural Language :: English",
-    "License :: OSI Approved :: MIT License",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-]
-
-
-def getPackageInfo():
-    info_dict = {}
-
-    # Info
-    readme = ""
-    if os.path.exists("README.rst"):
-        with io.open("README.rst", encoding='utf-8') as readme_file:
-            readme = readme_file.read()
-    hist = "`changelog <https://github.com/nicfit/nicfit.py/blob/master/HISTORY.rst>`_"
-    info_dict["long_description"] =\
-        readme + "\n\n" +\
-        "See the {} file for release history and changes.".format(hist)
-
-    return info_dict
-
-
-about_file = Path(__file__).parent / "nicfit/__about__.py"
-PKG_INFO = parcyl.setupAttrFromInfoFile(about_file)
-PKG_INFO.update(getPackageInfo())
-
-if PKG_INFO["release"].startswith("a"):
-    #classifiers.append("Development Status :: 1 - Planning")
-    #classifiers.append("Development Status :: 2 - Pre-Alpha")
-    classifiers.append("Development Status :: 3 - Alpha")
-elif PKG_INFO["release"].startswith("b"):
-    classifiers.append("Development Status :: 4 - Beta")
-else:
-    classifiers.append("Development Status :: 5 - Production/Stable")
-    #classifiers.append("Development Status :: 6 - Mature")
-    #classifiers.append("Development Status :: 7 - Inactive")
-
-gz = "{name}-{version}.tar.gz".format(**PKG_INFO)
-PKG_INFO["download_url"] = (
-    "{github_url}/releases/downloads/v{version}/{gz}"
-    .format(gz=gz, **PKG_INFO)
+setup = Setup(info_file="nicfit/__about__.py").with_packages(".", exclude=["test", "test.*"])
+setup(package_dir={"": "."},
+      zip_safe=False,
+      platforms=["Any"],
+      test_suite="./tests",
+      include_package_data=True,
+      package_data={
+          "mishmash": ["alembic.ini"] + find_package_files("mishmash/alembic"),
+          "mishmash.web": find_package_files("mishmash/web/static", "../..")
+                          + find_package_files("mishmash/web/templates", "../.."),
+          },
+      entry_points={
+                  "console_scripts": [
+                      "mishmash = mishmash.__main__:app.run",
+                  ]
+              },
 )
-
-
-def package_files(directory, prefix=".."):
-    paths = []
-    for (path, _, filenames) in os.walk(directory):
-        if "__pycache__" in path:
-            continue
-        for filename in filenames:
-            if filename.endswith(".pyc"):
-                continue
-            paths.append(os.path.join(prefix, path, filename))
-    return paths
-
-
-if sys.argv[1:] and sys.argv[1] == "--release-name":
-    print(PKG_INFO["release_name"])
-    sys.exit(0)
-else:
-    # The extra command line options we added cause warnings, quell that.
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", message="Unknown distribution option")
-        warnings.filterwarnings("ignore", message="Normalizing")
-
-        setup = parcyl.Setup(classifiers=classifiers,
-                             zip_safe=False,
-                             platforms=["Any"],
-                             keywords=["application", "commands",
-                                       "cookiecutter", "utils"],
-                             test_suite="./tests",
-                             include_package_data=True,
-                             package_data={
-                                 "nicfit": package_files("cookiecutter/"),
-                             },
-                             entry_points={
-                                 "console_scripts": [
-                                     "nicfit = nicfit.__main__:app.run",
-                                 ]
-                             },
-                             **PKG_INFO).with_packages(".", exclude=["tests",
-                                                                     "tests.*"])
-        setup()
